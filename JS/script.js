edfa08ca7886e9b8e106fd02e14c5290de40346e
LupeF <46634202+LupeF@users.noreply.github.com>
2023-12-29 12:45:04 -0800

    add evemt listeners to buttons

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,14 @@
 const button = document.querySelector(".button-container");
 const backSide = document.querySelector(".card");
-const backButton =document.querySelector(".back-button")
+const backButton =document.querySelector(".back-button");
 button.addEventListener('click',()=>{
     backSide.classList.toggle("flip-back");
+    // backSide.classList.toggle("flip-front");
 })
 
-// backButton.addEventListener('click',()=>{
-//     backSide.classList.toggle("flip-back");
-//     backSide.classList.toggle("flip-front");
-// })
+backButton.addEventListener('click',()=>{
+    backSide.classList.toggle("flip-back");
+    backSide.classList.toggle("flip-front");
+})
 // const reverse = document.querySelector('card')
 
