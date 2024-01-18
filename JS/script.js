b76f8522baf0e63d2a8d72a79ad5530a49028754
LupeF <46634202+LupeF@users.noreply.github.com>
2024-01-18 13:34:25 -0800

    fix flipping effect and styles

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,11 @@
 const button = document.querySelector(".button-container");
+// const backSide = document.querySelector(".card");
 const backSide = document.querySelector(".card");
+// const profile = document.querySelector("section");
 const backButton =document.querySelector(".back-button");
 button.addEventListener('click',()=>{
     backSide.classList.toggle("flip-back");
+    profile.classList.toggle("flip-back")
     // backSide.classList.toggle("flip-front");
 })
 
