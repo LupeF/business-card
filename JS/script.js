b1e30c6738cd77d07e39c70c5f6cd507e56a64cb
LupeF <46634202+LupeF@users.noreply.github.com>
2024-01-25 23:24:22 -0800

    fix JS indentation

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,14 @@
+const card = document.querySelector(".card");
 const button = document.querySelector(".button-container");
-// const backSide = document.querySelector(".card");
-const backSide = document.querySelector(".card");
-// const profile = document.querySelector("section");
 const backButton =document.querySelector(".back-button");
+
 button.addEventListener('click',()=>{
-    backSide.classList.toggle("flip-back");
-    profile.classList.toggle("flip-back")
-    // backSide.classList.toggle("flip-front");
+    card.classList.toggle("flip-back");
 })
 
 backButton.addEventListener('click',()=>{
-    backSide.classList.toggle("flip-back");
-    backSide.classList.toggle("flip-front");
+    card.classList.toggle("flip-back");
+    
 })
-// const reverse = document.querySelector('card')
+
 
